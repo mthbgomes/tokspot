3ce5798eb33ddd1a28f775d820c8068a9edf10bb
Matheus B. Gomes <matheusbgomes@hotmail.com>
2023-09-24 18:15:25 -0300

    feat: kanban columns  and draggable cards

--- js/drag.js
+++ js/drag.js
@@ -1,13 +1,31 @@
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
+// Initialize Draggable.js for the cards within the columns
+const kanbanCards = new Draggable.Sortable(
+  document.querySelectorAll(".kanban-card-container"),
+  {
+    draggable: ".kanban-card",
+    mirror: {
+      appendTo: "body",
+      constrainDimensions: true,
+    },
+  }
+);
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+// Add an event listener to handle when a card is dropped
+kanbanCards.on("sortable:stop", (event) => {
+  // Check if the card has an 'id' property and the 'newContainer' is defined
+  if (
+    event.data.originalSource &&
+    event.data.originalSource.id &&
+    event.newContainer
+  ) {
+    const cardId = event.data.originalSource.id;
+    const targetColumn = event.newContainer.closest(".kanban-col");
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.currentTarget.appendChild(document.getElementById(data));
-}
+    if (targetColumn) {
+      // Check if the card was dropped into a valid column (Lead or Primeiro Contato)
+      targetColumn
+        .querySelector(".kanban-card-container")
+        .appendChild(document.getElementById(cardId));
+    }
+  }
+});
