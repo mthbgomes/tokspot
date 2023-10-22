483993abc10fba3a7280edfb9c285cd2635b4f33
Matheus <matheusbgomes@hotmail.com>
2023-10-22 20:03:39 -0300

    feat: add customerSchema

---
+++ server/src/models/customerSchema.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const customerSchema = new Schema(
+  {
+    _id: Schema.Types.ObjetcId,
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: [
+        /\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[0-9])[0-9]{3}\-?[0-9]{4}$/,
+      ],
+    },
+    email: {
+      type: String,
+      required: true,
+      match: [/\S+@\S+\.\S+/, "Digite um e-mail váldio"],
+      lowercase: true,
+    },
+    opportunity: [String],
+    task,
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+export default Customer;
