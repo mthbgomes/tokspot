e594fb6a62dd67e61025bdf9cb6ac044ff1569cb
mthbgomes <matheusbgomes@hotmail.com>
2023-10-25 15:51:37 -0300

    Remove unused code

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
@@ -7,7 +7,7 @@ const Login = (props) => {
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
 
   const onButtonClick = () => {
     // Set initial error values to empty
